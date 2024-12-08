fab381abeb3bf804dbe0db99877e425a87c7e9c9
Kavya <135150115+CaptainCode2024@users.noreply.github.com>
2024-12-08 21:24:55 +0530

    Updated paths in static and template files.
    - Updated path to static files as per flask requirement.
    - Commented out route for rainfall analysis as it's not currently working.
    - Some other important changes.

--- SourceCode/app.py
+++ SourceCode/app.py
@@ -1,16 +1,14 @@
 import sys
 import os
-
-# Add the project directory to the Python path
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
-# Import the function from rainfall_analysis.py
-from rainfall_analysis import predict_rainfall
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = '5791628bb0b13ce0c676dfde280ba245'
+
+
+# Import the function from rainfall_analysis.py
+#from rainfall_analysis import predict_rainfall
+
 
 @app.route('/')
 def index():
@@ -24,19 +22,19 @@ def rainfall():
 def rainfall_entry():
     return render_template('rainfall_entry.html')
 
-@app.route('/rainfall_result', methods=['POST', 'GET'])
-def rainfall_result():
-    if request.method == 'POST':
-        if len(request.form['Year']) == 0:
-            flash("Please Enter Data!!")
-            return redirect(url_for('rainfall_entry'))
-        else:
-            year = request.form['Year']
-            region = request.form['SEL']
-            mae, score = predict_rainfall(year, region)
-            return render_template('rainfall_result.html', Mae=mae, Score=score)
-    else:
-        return redirect(url_for('rainfall_entry'))
+#@app.route('/rainfall_result', methods=['POST', 'GET'])
+#def rainfall_result():
+#    if request.method == 'POST':
+#        if len(request.form['Year']) == 0:
+#            flash("Please Enter Data!!")
+#            return redirect(url_for('rainfall_entry'))
+#        else:
+#            year = request.form['Year']
+#            region = request.form['SEL']
+#            mae, score = predict_rainfall(year, region)
+#            return render_template('rainfall_result.html', Mae=mae, Score=score)
+#    else:
+#        return redirect(url_for('rainfall_entry'))
 
 @app.route('/emergency_contacts')
 def emergency_contacts():
