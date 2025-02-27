d40a89e9224612241bf2576f56547699b2b20748
Kavya <135150115+CaptainCode2024@users.noreply.github.com>
2025-02-27 19:53:47 +0530

    Changes

--- SourceCode/static/js/main.js
+++ SourceCode/static/js/main.js
@@ -188,3 +188,14 @@
 			});
 
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a.scrolly').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
